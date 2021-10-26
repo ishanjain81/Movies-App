13ba1ee52010ab63efcf721a45711d0c823b6277
Ishan Jain <ishanjaincool81@gmail.com>
2021-10-26 20:47:29 +0530

    Adding Css for mobile view in Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ export default class Navbar extends Component {
     render() {
         return (
             <div className="Navbarr">
-                <Link to="/" style={{textDecoration:'none'}}><h1>Movies</h1></Link>
+                <Link to="/" className="NavbarTitle" style={{textDecoration:'none'}}><h1>Movies</h1></Link>
                 <Link to="/favourites" style={{textDecoration:'none'}}><h2 style={{marginLeft:'2rem',marginTop:'0.5rem'}}>Favourites</h2></Link>
             </div>
         )
